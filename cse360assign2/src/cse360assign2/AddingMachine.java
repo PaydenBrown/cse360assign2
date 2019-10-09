8924df1abaf65cf2668c7b789e69f816ea9aa901
Home <Home@users-air.3500.dhcp.asu.edu>
2019-10-09 11:25:27 -0700

    First commit

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,37 @@
+// author: Payden Brown
+// class ID: CSE360 
+// assignment 2
+// Description: AddingMachine works like a calculator that adds and 
+// subtracts input values from a total value. There is also a clear
+// and print option.   
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
